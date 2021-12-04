8d073ea94ef5aba49c6b02e1b84157e10a48c185
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2021-12-03 21:26:31 -0800

    feat: build a connect wallet button

--- buildspace/src/App.js
+++ buildspace/src/App.js
@@ -1,45 +1,39 @@
-import React, { useState, useEffect } from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
 
-import ABI from "./utils/WavePortal.json";
-
-import "./styles/App.css";
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
-  const [allWaves, setAllWaves] = useState([]);
-  const [waveCount, setWaveCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [waveMessage, setWaveMessage] = useState("");
-
-  const contractAddress = "0x00aeCA37220dBEA2c6D2dB3243b7b1CE8fDC9423";
-  const contractABI = ABI.abi;
-
+  
   const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
+    const { ethereum } = window;
 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
-
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
-      } else {
-        console.log("No authorized account found");
-      }
-    } catch (error) {
-      console.log(error);
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
     }
-  };
 
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  /*
+  * Implement your connectWallet method here
+  */
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -49,121 +43,60 @@ const App = () => {
         return;
       }
 
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
+      setCurrentAccount(accounts[0]); 
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  const getWavePortalContract = async () => {
-    const { ethereum } = window;
-    if (ethereum) {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const wavePortalContract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
-      return wavePortalContract;
-    }
-  };
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
 
   useEffect(() => {
     checkIfWalletIsConnected();
-    async function getContract() {
-      const wavePortalContract = await getWavePortalContract();
-      const count = await wavePortalContract.getTotalWaves();
-      const waves = await wavePortalContract.getAllWaves();
-      setWaveCount(count.toNumber());
-      let wavesCleaned = [];
-      waves.forEach((wave) => {
-        wavesCleaned.push({
-          address: wave.waver,
-          timestamp: new Date(wave.timestamp * 1000),
-          message: wave.message,
-        });
-      });
-      setAllWaves(wavesCleaned);
-    }
-    getContract();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
-  const wave = async () => {
-    try {
-      const { ethereum } = window;
-
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-
-      if (accounts.length === 0) {
-        await connectWallet();
-      }
-
-      const wavePortalContract = await getWavePortalContract();
-
-      let count = await wavePortalContract.getTotalWaves();
-      setWaveCount(count.toNumber());
-      console.log("Retrieved total wave count...", count.toNumber());
-      const waveTxn = await wavePortalContract.wave(waveMessage, { gasLimit: 300000 });
-      console.log("Mining...", waveTxn.hash);
-      setLoading(true);
-
-      await waveTxn.wait();
-      console.log("Mined -- ", waveTxn.hash);
-      setLoading(false);
-      setWaveMessage("");
-      count = await wavePortalContract.getTotalWaves();
-      console.log("Retrieved total wave count...", count.toNumber());
-      setWaveCount(count.toNumber());
-      const allWaves = await wavePortalContract.getAllWaves();
-      setAllWaves(allWaves)
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already conencted :).
+  */
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
-        <input
-          onChange={(e) => setWaveMessage(e.target.value)}
-          placeholder="Send a message / link"
-        />
-        <button className="waveButton" onClick={() => wave()}>
-          Wave at Me
-        </button>
-        <div>Total Wave Count: {waveCount}</div>
-        {loading ? <div>Mining...</div> : null}
-        {!currentAccount ? (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        ) : (
-          <div className="bio">Connected {currentAccount}</div>
-        )}
-        {allWaves.map((wave, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                backgroundColor: "OldLace",
-                marginTop: "16px",
-                padding: "8px",
-              }}
-            >
-              <div>Address: {wave.address}</div>
-              <div>Time: {wave.timestamp.toString()}</div>
-              <div>Message: {wave.message}</div>
-            </div>
-          );
-        })}
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
     </div>
   );
